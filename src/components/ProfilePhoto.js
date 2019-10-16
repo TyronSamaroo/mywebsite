74c3e76306fd8fa3ed0829762d7e15350aefc14e
Tyron Samaroo <tyronsamaroo@gmail.com>
2019-10-16 17:18:18 -0400

    Fix Picture

--- src/components/ProfilePhoto.js
+++ src/components/ProfilePhoto.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import mypic from 'images/tyronpic.jpeg'
+//import mypic from './images/tyronpic.jpeg';
 const Styles = styled.div`
 height: 100;
 radius: 50;
+text-align:center;
 
 
 	
@@ -12,9 +13,9 @@ radius: 50;
 export const ProfilePhoto = () => {
     return (
         <div>
-            <h2>Tyron Samaroo</h2>
+            <h2>Tyron Samarooo</h2>
             <Styles>
-                <img src={mypic} alt={"mypic" />
+                <img src={'./images/tyronpic.jpeg'} alt={"mypic" }/>
             </Styles>
         </div>
     )
